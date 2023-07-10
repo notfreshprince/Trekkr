8f7c223a9e5cffc54e382568640bcef14cfb0d19
Ash Prince <ashrprince@icloud.com>
2023-07-10 17:01:36 +0100

    Initial Commit

---
+++ Trekkr/TrekkrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekkrApp.swift
+//  Trekkr
+//
+//  Created by Ash Prince on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrekkrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
